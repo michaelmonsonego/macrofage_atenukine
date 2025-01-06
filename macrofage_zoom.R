b8df9787a24d35931c3e10a9918f134bbffd26e8
michaelmonsonego <142400573+michaelmonsonego@users.noreply.github.com>
2025-01-06 15:52:04 +0200

    update

--- macrofage_zoom.R
+++ macrofage_zoom.R
@@ -22,7 +22,7 @@ library(snakecase)
 library(glue)
 library(ggrepel)
 
-setwd("D:/Michael/git_check/atteno_macrofage")
+setwd("D:/Michael/git_check/macrofage_atenukine")
 
 x1=4.6
 #T_cells = readRDS("objects/tils_all_.45_integrgate_annotated (2).rds")
@@ -33,9 +33,9 @@ DimPlot(myeloid, reduction = "tsne", label = TRUE, pt.size = 0.5, label.size = 1
   ggplot2::theme(
     axis.title.x = element_text(size = 30),  # X-axis title size
     axis.title.y = element_text(size = 30),  # Y-axis title size
-    axis.text.x = element_text(size = 24),                 # X-axis tick labels size
-    axis.text.y = element_text(size = 24),                 # Y-axis tick labels size
-    axis.ticks = element_line(size = 1)                    # Adjust axis tick size
+    axis.text.x = element_text(size = 24),   # X-axis tick labels size
+    axis.text.y = element_text(size = 24),   # Y-axis tick labels size
+    axis.ticks = element_line(size = 1)      # Adjust axis tick size
   )
 
 markers_per_cluster <- FindAllMarkers(myeloid, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
@@ -82,7 +82,7 @@ tSNE_ls <- list(tSNE_0.05,tSNE_0.3,tSNE_0.35,tSNE_0.4,tSNE_0.45,tSNE_0.5)
 all_tSNE <- plot_grid(tSNE_0.05,tSNE_0.3,tSNE_0.35,tSNE_0.4,tSNE_0.45,tSNE_0.5, ncol = 3) 
 ggsave(all_tSNE,filename = 'figures/macrofage_zoom/macrofage_zoom_0_14_dim_res_test.png', dpi=300, height=10, width=16)
 
-macrofages <- FindClusters(macrofages, resolution = 0.35) #M# 6.1.25 choose madafaka
+macrofages <- FindClusters(macrofages, resolution = 0.35)
 # saveRDS(macrofages, file = "objects/macrofage_zoom_.35.rds") #M# change if adjusted
 macrofages <- readRDS("objects/macrofage_zoom_.35.rds")
 
@@ -97,11 +97,32 @@ write_csv(Top50Markers, "Excels/macrofage_zoom_DE genes.csv")
 
 
 #M# DE genes between groups, not clusters
-macrofages <- SetIdent(macrofages, value = "Sample") #M# change back to seurat clusters ?
+macrofages <- SetIdent(macrofages, value = "Sample") #M# this enables DE genes between each group to the rest, change back to seurat clusters after running:
+#M# macrofages <- SetIdent(macrofages, value = "seurat_clusters")
 DE_all <- FindAllMarkers(macrofages, only.pos = TRUE)
+top_100_per_group <-  DE_all %>% 
+  group_by(cluster) %>% 
+  top_n(n = 100, wt = avg_log2FC) %>% 
+  as.data.frame %>% 
+  arrange(cluster,-avg_log2FC)
+
+write_csv(top_100_per_group, "Excels/DE_between_groups.csv")
 
 
+DimPlot(macrofages, reduction = "tsne", label = TRUE, pt.size = 0.5, label.size = 10) +
+  ggplot2::theme(legend.position = "none") +
+  ggplot2::theme(
+    axis.title.x = element_text(size = 30),  # X-axis title size
+    axis.title.y = element_text(size = 30),  # Y-axis title size
+    axis.text.x = element_text(size = 24),   # X-axis tick labels size
+    axis.text.y = element_text(size = 24),   # Y-axis tick labels size
+    axis.ticks = element_line(size = 1)      # Adjust axis tick size
+  )
+ggsave(file = "figures/macrofage_zoom/tsne_no_annotation.png", dpi=300, width=10, height=10)
 
+
+FeaturePlot(macrofages, features = c("Mki67"), order=TRUE,pt.size=0.5, reduction="tsne", ncol=1)
+ggsave(file = "figures/macrofage_zoom/proliferation.png", dpi=300, width=4, height=4)
 
 
 
